ebe570ea230beeba61b998a0f12e84d24907fd2b
Dan Hall <dhall0803@gmail.com>
2021-04-11 21:57:12 +0100

    refined

--- table.js
+++ table.js
@@ -1,9 +1,13 @@
-$.get('https://3zisga6su2.execute-api.eu-west-2.amazonaws.com/gigs', function (data) {
+jQuery.get('https://3zisga6su2.execute-api.eu-west-2.amazonaws.com/gigs', function (data) {
     var json = JSON.parse(data);
     var html = "";
     for (var i = 0; i < json.length; i++) {
-        html = html.concat('<tr><td>', json[i]['date'], '</tr></td>');
-        html = html.concat('<tr><td>', json[i]['title'], '</tr></td>');
+        var splitDate = json[i]['date'].split("-");
+        var year = splitDate[0]
+        var month = splitDate[1]
+        var day = splitDate[2]
+        var dateObj = new Date(year, month -1, day)
+        html = html.concat('<tr class="white-bg"><td>', dateObj.toDateString().substring(4), '</tr></td>', '<tr class="black-bg"><td>', json[i]['title'], '</tr></td>');
     }
-    $('#JS_Table_Content').html(html); 
+    jQuery('#JS_Table_Content').html(html); 
 })
